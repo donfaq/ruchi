154f0e19f4e160293b4322ad4dbd6a4e4d535eb1
Grigory Zhigalov <zhigalov8@gmail.com>
2019-09-22 16:30:43 +0300

    sl4j logging

--- src/main/java/com/donfaq/Start.java
+++ src/main/java/com/donfaq/Start.java
@@ -1,13 +1,10 @@
 package com.donfaq;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * Hello world!
- */
 public class Start {
-    private static final Logger log = LogManager.getLogger(Start.class);
+    private static final Logger log = LoggerFactory.getLogger(Start.class);
 
 
     public static void main(String[] args) {
