67c9d61a24c7627bef215fa35c3aeacf1952d9f5
Grigory Zhigalov <zhigalov8@gmail.com>
2019-09-29 22:51:23 +0300

    discord permissions check

--- src/main/java/com/donfaq/ruchi/integration/service/DiscordServiceImpl.java
+++ src/main/java/com/donfaq/ruchi/integration/service/DiscordServiceImpl.java
@@ -1,6 +1,7 @@
 package com.donfaq.ruchi.integration.service;
 
 import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.TextChannel;
@@ -17,7 +18,7 @@ import java.util.Deque;
 @Service
 public class DiscordServiceImpl implements DiscordService {
     private final Logger log = LoggerFactory.getLogger(DiscordServiceImpl.class);
-
+    private JDA jda;
     private TextChannel textChannel;
     private Deque<String> sentMessages;
 
@@ -26,13 +27,23 @@ public class DiscordServiceImpl implements DiscordService {
             @Value("${discord.botToken}") final String botToken,
             @Value("${discord.channelId}") final long channelId
     ) throws LoginException, InterruptedException {
-        textChannel = (TextChannel) new JDABuilder(AccountType.BOT)
+        log.info("Connecting to Discord bot");
+        jda = new JDABuilder(AccountType.BOT)
                 .setToken(botToken)
                 .build()
-                .awaitReady()
-                .getGuildChannelById(ChannelType.TEXT, channelId);
+                .awaitReady();
 
+        log.info("Trying to reach specified channel");
+        textChannel = (TextChannel) jda.getGuildChannelById(ChannelType.TEXT, channelId);
+
+        if (textChannel == null) {
+            throw new Error("Can't find Discord channel by ID");
+        }
+        if (!textChannel.canTalk()) {
+            throw new Error("Insufficient permissions in specified channel. Check Read and Write messages permissions");
+        }
         this.sentMessages = new ArrayDeque<>(10);
+        log.info("Discord Service ready");
     }
 
     private synchronized boolean isSentEarlier(String message) {
