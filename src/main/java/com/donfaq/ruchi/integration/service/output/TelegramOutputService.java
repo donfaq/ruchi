91695657bd8510fcdb6dc9393afa338fde820536
Grigory Zhigalov <zhigalov8@gmail.com>
2020-07-15 21:50:43 +0300

    photo

--- src/main/java/com/donfaq/ruchi/integration/service/output/TelegramOutputService.java
+++ src/main/java/com/donfaq/ruchi/integration/service/output/TelegramOutputService.java
@@ -5,6 +5,7 @@ import com.pengrad.telegrambot.Callback;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.request.ParseMode;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.request.SendPhoto;
 import com.pengrad.telegrambot.response.SendResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,8 +21,8 @@ public class TelegramOutputService implements OutputService {
     private final String tgChannelId;
 
     public TelegramOutputService(
-                @Value("${telegram.botToken}") String tgBotToken,
-                @Value("${telegram.channelId}") String tgChannelId
+            @Value("${telegram.botToken}") String tgBotToken,
+            @Value("${telegram.channelId}") String tgChannelId
     ) {
         this.bot = new TelegramBot(tgBotToken);
         this.tgChannelId = tgChannelId;
@@ -29,21 +30,40 @@ public class TelegramOutputService implements OutputService {
 
     @Override
     public void send(BroadcastMessage message) {
-        SendMessage request = new SendMessage(tgChannelId, message.getText())
-                .parseMode(ParseMode.HTML)
-                .disableWebPagePreview(true)
-                .disableNotification(true);
 
-        bot.execute(request, new Callback<SendMessage, SendResponse>() {
-            @Override
-            public void onResponse(SendMessage request, SendResponse response) {
-                log.info("Message sent to Telegram");
-            }
 
-            @Override
-            public void onFailure(SendMessage request, IOException e) {
-                log.error("Error while sending message to Telegram", e);
-            }
-        });
+        if (!message.getImages().isEmpty()) {
+            SendPhoto request = new SendPhoto(tgChannelId, message.getImages().get(0).toString())
+                    .caption(message.getText());
+
+            bot.execute(request, new Callback<SendPhoto, SendResponse>() {
+                @Override
+                public void onResponse(SendPhoto request, SendResponse response) {
+                    log.info("Message sent to Telegram");
+                }
+
+                @Override
+                public void onFailure(SendPhoto request, IOException e) {
+                    log.error("Error while sending message to Telegram", e);
+                }
+            });
+        } else {
+            SendMessage request = new SendMessage(tgChannelId, message.getText())
+                    .parseMode(ParseMode.HTML)
+                    .disableWebPagePreview(true);
+            bot.execute(request, new Callback<SendMessage, SendResponse>() {
+                @Override
+                public void onResponse(SendMessage request, SendResponse response) {
+                    log.info("Message sent to Telegram");
+                }
+
+                @Override
+                public void onFailure(SendMessage request, IOException e) {
+                    log.error("Error while sending message to Telegram", e);
+                }
+            });
+        }
+
+
     }
 }
