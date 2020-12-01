b22d7cde9b2865483668e3820eef6bca29c004a2
Grigory Zhigalov <zhigalov8@gmail.com>
2020-12-02 01:56:45 +0300

    fix websub request params

--- src/main/java/com/donfaq/ruchi/config/WebConfig.java
+++ src/main/java/com/donfaq/ruchi/config/WebConfig.java
@@ -1,23 +1,16 @@
 package com.donfaq.ruchi.config;
 
-import com.donfaq.ruchi.component.converters.StringToWebSubHubModeConverter;
 import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToWebSubHubModeConverter());
-    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
