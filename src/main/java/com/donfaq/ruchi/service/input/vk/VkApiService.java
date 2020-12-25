297a7d637ed3f08cd696408476c17f240ceadd1d
Grigory Zhigalov <zhigalov8@gmail.com>
2020-12-25 18:21:30 +0300

    vk-sdk fixes

--- src/main/java/com/donfaq/ruchi/service/input/vk/VkApiService.java
+++ src/main/java/com/donfaq/ruchi/service/input/vk/VkApiService.java
@@ -2,13 +2,12 @@ package com.donfaq.ruchi.service.input.vk;
 
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.ServiceActor;
-import com.vk.api.sdk.objects.photos.Photo;
+import com.vk.api.sdk.objects.photos.responses.GetByIdResponse;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class VkApiService {
@@ -20,16 +19,8 @@ public class VkApiService {
         this.vkApiClient = vkApiClient;
     }
 
-    public List<Photo> updatePhotoInfo(List<Photo> photos) {
-        return getPhotoById(
-                photos.stream()
-                      .map(photo -> String.format("%s_%s", photo.getOwnerId(), photo.getId()))
-                      .collect(Collectors.toList())
-        );
-    }
-
     @SneakyThrows
-    public List<Photo> getPhotoById(List<String> photos) {
+    public List<GetByIdResponse> getPhotoById(List<String> photos) {
         return vkApiClient.photos()
                           .getById(new ServiceActor(null, serviceAccessToken), photos)
                           .execute();
