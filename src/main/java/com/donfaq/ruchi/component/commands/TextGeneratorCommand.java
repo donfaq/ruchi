084265f75428cf6035f286b7f510290af1578734
Grigory Zhigalov <zhigalov8@gmail.com>
2021-07-22 16:53:24 +0300

    wip

--- src/main/java/com/donfaq/ruchi/component/commands/TextGeneratorCommand.java
+++ src/main/java/com/donfaq/ruchi/component/commands/TextGeneratorCommand.java
@@ -12,24 +12,42 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class TextGeneratorCommand {
     private final TextGeneratorClient textGeneratorClient;
+    private static final Integer DEFAULT_MAX_LENGTH = 240;
 
-    private Optional<String> generateText(String model, String query) {
-        return Optional.of(textGeneratorClient.generateText(model, query, 240));
+    private Optional<String> generateText(String model, String query, Integer maxLenght) {
+        return Optional.of(textGeneratorClient.generateText(model, query, maxLenght));
+    }
+
+    public Optional<String> speak(String query, Integer maxLength) {
+        return generateText("chat", query, maxLength);
     }
 
     public Optional<String> speak(String query) {
-        return generateText("chat", query);
+        return generateText("chat", query, DEFAULT_MAX_LENGTH);
+    }
+
+    public Optional<String> pron(String query, Integer maxLength) {
+        return generateText("pron", query, maxLength);
     }
 
     public Optional<String> pron(String query) {
-        return generateText("pron", query);
+        return generateText("pron", query, DEFAULT_MAX_LENGTH);
+    }
+
+    public Optional<String> gachi(String query, Integer maxLength) {
+        return generateText("gachi", query, maxLength);
     }
 
     public Optional<String> gachi(String query) {
-        return generateText("gachi", query);
+        return generateText("gachi", query, DEFAULT_MAX_LENGTH);
+    }
+
+    public Optional<String> kalik(String query, Integer maxLength) {
+        return generateText("kalik", query, maxLength);
     }
 
     public Optional<String> kalik(String query) {
-        return generateText("kalik", query);
+        return generateText("kalik", query, DEFAULT_MAX_LENGTH);
     }
+
 }
