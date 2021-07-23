fdd4e118feafcdc9a1ed7a442ec568dce1ebdb83
Grigory Zhigalov <zhigalov8@gmail.com>
2021-07-23 22:07:39 +0300

    add textgen controller (#137)

---
+++ src/main/java/com/donfaq/ruchi/service/TextgenService.java
@@ -0,0 +1,32 @@
+package com.donfaq.ruchi.service;
+
+import com.donfaq.ruchi.component.commands.TextGeneratorCommand;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class TextgenService {
+    private final TextGeneratorCommand textGen;
+
+    public String generate(String model) {
+        String defaultResponse = "Что-то пошло не так";
+        String responseMessage = defaultResponse;
+
+        try {
+            responseMessage = switch (model) {
+                case "speak" -> textGen.speak("").orElse(defaultResponse);
+                case "pron" -> textGen.pron("").orElse(defaultResponse);
+                case "gachi" -> textGen.gachi("").orElse(defaultResponse);
+                case "kalik" -> textGen.kalik("").orElse(defaultResponse);
+                case "woman" -> textGen.woman("").orElse(defaultResponse);
+                default -> defaultResponse;
+            };
+        } catch (Exception e) {
+            log.warn("Exception during response generation", e);
+        }
+        return responseMessage;
+    }
+}
