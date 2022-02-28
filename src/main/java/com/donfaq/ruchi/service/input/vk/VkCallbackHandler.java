e893dbffc657609734beb86e9ee6ff47d78639e9
donfaq <zhigalov8@gmail.com>
2022-02-28 23:01:38 +0300

    update vk, twitch and telegram lib versions

--- src/main/java/com/donfaq/ruchi/service/input/vk/VkCallbackHandler.java
+++ src/main/java/com/donfaq/ruchi/service/input/vk/VkCallbackHandler.java
@@ -3,14 +3,11 @@ package com.donfaq.ruchi.service.input.vk;
 import com.donfaq.ruchi.config.properties.VkConfigProperties;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
+import com.vk.api.sdk.events.Events;
 import com.vk.api.sdk.objects.callback.messages.CallbackMessage;
 import com.vk.api.sdk.objects.wall.Wallpost;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-
-import java.lang.reflect.Type;
-import java.util.Map;
 
 @Slf4j
 @Component
@@ -24,41 +21,36 @@ public class VkCallbackHandler {
     }
 
     private final Gson gson = new Gson();
-    private final static Map<String, Type> CALLBACK_TYPES;
-    private static final String CALLBACK_EVENT_WALL_POST_NEW = "wall_post_new";
-    private static final String CALLBACK_EVENT_CONFIRMATION = "confirmation";
     private static final String OK_RESPONSE = "ok";
-
-    static {
-        CALLBACK_TYPES = Map.of(CALLBACK_EVENT_WALL_POST_NEW, new TypeToken<CallbackMessage<Wallpost>>() {}.getType());
-    }
 
     private void wallPostNew(Wallpost wallpost) {
         wallpostProcessor.process(wallpost);
     }
 
     public String parse(String json) {
-        JsonObject jsonObject = gson.fromJson(json, JsonObject.class);
-        return parse(jsonObject);
+        return parse(gson.fromJson(json, CallbackMessage.class));
     }
 
-    public String parse(JsonObject json) {
-        String type = json.get("type").getAsString();
-        if (type.equalsIgnoreCase(CALLBACK_EVENT_CONFIRMATION)) {
-            return this.vkConfig.getConfirmationCode();
-        }
-        Type typeOfClass = CALLBACK_TYPES.get(type);
-        if (typeOfClass == null) {
-            log.warn("Unsupported callback event: {}", type);
-        }
+    private <T> T designateObject(JsonObject object, Events type) {
+        return gson.fromJson(object, type.getType());
+    }
 
-        if (CALLBACK_EVENT_WALL_POST_NEW.equals(type)) {
-            CallbackMessage<Wallpost> message = gson.fromJson(json, typeOfClass);
-            wallPostNew(message.getObject());
+    public String parse(CallbackMessage message) {
+        switch (message.getType()) {
+            case CONFIRMATION:
+                return confirmation();
+            case WALL_POST_NEW:
+                wallPostNew(designateObject(message.getObject(), message.getType()));
+                break;
+            default:
+                log.error("Unexpected callback event type received");
+                return null;
         }
-
         return OK_RESPONSE;
     }
 
+    private String confirmation() {
+        return this.vkConfig.getConfirmationCode();
+    }
 
 }
